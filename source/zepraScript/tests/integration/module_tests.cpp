c2e86d31185d0d3714ff9e546fd826eefcf44e03
Swana <metaketivee@gmail.com>
2026-03-15 18:09:03 +0530

    refactor: remove ZepraScript tests and update documentation.

--- source/zepraScript/tests/integration/module_tests.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2025 KetiveeAI. All rights reserved.
-// Licensed under KPL-2.0. See LICENSE file for details.
-// Integration tests for ZepraScript module system
-#include <gtest/gtest.h>
-#include "config.hpp"
-
-TEST(IntegrationTests, Placeholder) {
-    EXPECT_TRUE(true);
-}
--- source/zepraScript/tests/integration/promise_tests.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025 KetiveeAI. All rights reserved.
-// Licensed under KPL-2.0. See LICENSE file for details.
-#include <gtest/gtest.h>
-#include "config.hpp"
-
-TEST(PromiseTests, Placeholder) {
-    EXPECT_TRUE(true);
-}
